1746800ea95844a2cafc64c81d71b8e8fff73b2c
Asikur Rahaman <mdasikur5893@gmail.com>
2026-05-16 00:11:36 +0600

    favorite reviews show in ui

--- src/Pages/FavoritesReviews/FavoritesReviews.jsx
+++ src/Pages/FavoritesReviews/FavoritesReviews.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react';
 import useInstance from '../../Hooks/useInstance';
+import useAuth from '../../Hooks/useAuth';
 
 const FavoritesReviews = () => {
     const instance = useInstance()
+    const {user} = useAuth()
     useEffect(()=>{
-        instance('/favoritesReviewsColl')
+        instance(`/favoritesReviewsColl?email=${user.email}`)
         .then(res=>{
             console.log('all favorites reviews hre',res.data);
         }).catch(eror=>{
             console.log(eror);
         })
-    },[instance])
+    },[user,instance])
     return (
         <div>
             
